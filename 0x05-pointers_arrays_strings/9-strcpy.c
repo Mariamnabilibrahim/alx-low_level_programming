6254deb93fa43cfff9ef4c1a2af69b59246fcca0
Mariam <eng.mariamnabil93@gmail.com>
2023-05-02 11:02:12 -0700

    1

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcpy -  function that copies the string pointed to by src
+ *
+ * @dest: string
+ * @src: string
+ *
+ * Return: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = -1;
+
+	do {
+		x++;
+		dest[x] = scr[x];
+	}
+
+	while (src[x] != '\0');
+
+	return (dest);
+}
