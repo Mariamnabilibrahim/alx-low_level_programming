37ee11749787cf51e4cd557e788edf321c189ee5
Mariam <eng.mariamnabil93@gmail.com>
2023-05-04 07:07:21 -0700

    1

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *leet - function that encodes a string into 1337.
+ *
+ * @c: string
+ *
+ * Return: string
+*/
+
+char *leet(char *c)
+{
+	char *cp = c;
+	char Letter[] = {'A', 'E', 'O', 'T', 'L'};
+	int Num[] = {4, 3, 0, 7, 1};
+	unsigned int x;
+
+	while (*c)
+	{
+		for (x = 0; x < sizeof(Letter) / sizeof(char); x++)
+		{
+			if (*c == Letter[x] || *c == key[x] + 32)
+			{
+				*c = 48 + Num[x];
+			}
+		}
+		c++;
+	}
+
+	return (cp);
+}
