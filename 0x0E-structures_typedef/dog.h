de92f98233fab14bdadcca6334c043539abe4ba1
Mariam <eng.mariamnabil93@gmail.com>
2023-05-22 04:45:45 -0700

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: Define a new type struct dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
