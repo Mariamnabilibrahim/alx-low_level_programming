332b1822fa859f2cf1ae66d878a27c94eae66861
Mariam <eng.mariamnabil93@gmail.com>
2023-05-25 16:20:55 -0700

    1

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,51 +4,51 @@
 
 /**
  * format_char - formats of char
- * @separator: string
- * @ap: pointer
+ * @sep: string
+ * @p: pointer
 */
 
-void format_char(char *separator, va_list ap)
+void format_char(char *sep, va_list p)
 {
-	printf("%s%c", separator, va_arg(ap, int));
+	printf("%s%c", sep, va_arg(p, int));
 }
 
 /**
  * format_int - formats of int
- * @separator: string
- * @ap: pointer
+ * @sep: string
+ * @p: pointer
 */
 
-void format_int(char *separator, va_list ap)
+void format_int(char *sep, va_list p)
 {
-	printf("%s%d", separator, va_arg(ap, int));
+	printf("%s%d", sep, va_arg(p, int));
 }
 
 /**
  * format_float - formats of float
- * @separator: string
- * @ap: pointer
+ * @sep: string
+ * @p: pointer
 */
 
-void format_float(char *separator, va_list ap)
+void format_float(char *sep, va_list p)
 {
-	printf("%s%f", separator, va_arg(ap, double));
+	printf("%s%f", sep, va_arg(p, double));
 }
 
 /**
  * format_string - formats of string
- * @separator: string
- * @ap: pointer
+ * @sep: string
+ * @p: pointer
 */
 
-void format_string(char *separator, va_list ap)
+void format_string(char *sep, va_list p)
 {
-	char *s = va_arg(ap, char *);
+	char *s = va_arg(p, char *);
 
 	switch ((int)(!s))
 	case 1:
-		str = "(nil)";
-	printf("%s%s", separator, s);
+		s = "(nil)";
+	printf("%s%s", sep, s);
 }
 
 /**
@@ -59,8 +59,8 @@ void format_string(char *separator, va_list ap)
 void print_all(const char * const format, ...)
 {
 	int x = 0, y;
-	char *separator = "";
-	va_list ap;
+	char *sep = "";
+	va_list p;
 	token_t tokens[] = {
 		{"c", format_char},
 		{"i", format_int},
@@ -69,7 +69,7 @@ void print_all(const char * const format, ...)
 		{NULL, NULL}
 	};
 
-	va_start(ap, format);
+	va_start(p, format);
 	while (format && format[x])
 	{
 		y = 0;
@@ -77,13 +77,13 @@ void print_all(const char * const format, ...)
 		{
 			if (format[x] == tokens[y].token[0])
 			{
-				tokens[y].f(separator, ap);
-				separator = ", ";
+				tokens[y].f(sep, p);
+				sep = ", ";
 			}
 			y++
 		}
 		x++
 	}
 	printf("\n");
-	va_end(ap);
+	va_end(p);
 }
