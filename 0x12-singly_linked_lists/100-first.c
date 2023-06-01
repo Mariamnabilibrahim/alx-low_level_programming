19458358fc848a7ce75ba67bbc694819940710ad
Mariam <eng.mariamnabil93@gmail.com>
2023-06-01 06:07:18 -0700

    1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+void cons(void) __attribute__ ((cons));
+
+/**
+ * cons -  function that prints  before the main function is executed.
+ * 
+ * Return: void
+*/
+
+void cons(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
