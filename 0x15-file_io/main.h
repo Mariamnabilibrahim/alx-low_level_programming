f3c013f73b056fa0ebb308d46a657ddd3f4d0fdf
Mariam <eng.mariamnabil93@gmail.com>
2023-06-12 06:58:58 -0700

    1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,8 +9,11 @@
 #include <sys/stat.h>
 
 
+#define READ_BUFFER_SIZE 1024
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int _lenght(char *str);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
