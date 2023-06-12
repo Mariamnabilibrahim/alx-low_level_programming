3ba99b69d21e306f5e6850f5aaabbfd51f64aa0a
Mariam <eng.mariamnabil93@gmail.com>
2023-06-12 06:54:53 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,44 @@
 #include "main.h"
 
 /**
- * create_file -  function that creates a file.*/
+ * _lenght - function to find the lenght of string
+ * @str: string
+ * Return: the lenght
+*/
+
+int _lenght(char *str)
+{
+	int x = 0;
+
+	if (!str)
+		return (0);
+
+	while (*str++)
+		x++;
+	return (x);
+}
+
+/**
+ * create_file -  function that creates a file
+ * @filename:  is the name of the file
+ * @text_content: is a NULL terminated string to write to the file
+ *
+ * Returns: 1 on success, -1 on failure (file can not be created,
+ *	file can not be written, write “fails”, etc…)
+*/
 
 int create_file(const char *filename, char *text_content)
 {
+	int file;
+	ssize_t byte = 0, l = _lenght(text_conttent);
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
+	if (l)
+		byte = write(file, text_content, l);
+	close(file);
+	return (byte == l ? 1 : -1);
+}
